12f15429cf5e38cf5c740329d996b5e7d2df534a
Force Porquillo <fporquillo18@gmail.com>
2020-06-20 17:01:23 +0800

    move leak canary to application class

--- app/src/main/java/com/force/codes/project/app/app/DebugTreeApplication.java
+++ app/src/main/java/com/force/codes/project/app/app/DebugTreeApplication.java
@@ -5,7 +5,7 @@
  * Last modified 6/20/20 4:10 PM
  */
 
-package com.force.codes.project.app.presentation_layer.controller.support;
+package com.force.codes.project.app.app;
 
 import android.app.Application;
 import android.util.Log;
@@ -15,9 +15,12 @@ import com.force.codes.project.app.BuildConfig;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import leakcanary.AppWatcher;
+import leakcanary.LeakCanary;
+import leakcanary.ObjectWatcher;
 import timber.log.Timber;
 
-public class DebugTree extends Application{
+public class DebugTreeApplication extends Application{
 
     @Override
     public void onCreate(){
@@ -25,13 +28,16 @@ public class DebugTree extends Application{
 
         if(BuildConfig.DEBUG){
             Timber.plant(new Timber.DebugTree());
+            AppWatcher.getConfig().getWatchActivities();
+            AppWatcher.getConfig().getWatchFragments();
+            ObjectWatcher objectWatcher = AppWatcher.INSTANCE.getObjectWatcher();
+            objectWatcher.getRetainedObjectCount();
         }else{
             Timber.plant(new CrashReportingTree());
         }
     }
 
     private static class CrashReportingTree extends Timber.Tree{
-
         @Override
         protected void log(int priority, @Nullable String tag, @NotNull String message, @Nullable Throwable t){
             if(priority == Log.VERBOSE || priority == Log.DEBUG){
