336eac0d6a2b752c7bcf1aabd3e9ba1826160206
Force Porquillo <fporquillo18@gmail.com>
2020-06-23 04:20:49 +0800

    feat: added new thread executor pool for computation purposes

--- app/src/main/java/com/force/codes/project/app/service/executors/AppExecutors.java
+++ app/src/main/java/com/force/codes/project/app/service/executors/AppExecutors.java
@@ -1,9 +1,9 @@
 package com.force.codes.project.app.service.executors;
 
 /*
- * Created by Force Porquillo on 6/4/20 6:15 AM
+ * Created by Force Porquillo on 6/2/20 6:15 AM
  * Copyright (c) 2020.  All rights reserved.
- * Last modified 6/2/20 6:18 AM
+ * Last modified 6/4/20 6:18 AM
  *
  */
 
@@ -14,22 +14,27 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class AppExecutors{
     private static final int THREAD_COUNT = 3;
     private final Executor diskIO;
     private final Executor networkIO;
     private final Executor mainThread;
+    private final Executor computationThread;
 
-    private AppExecutors(Executor diskIO, Executor networkIO, Executor mainThread){
+
+    private AppExecutors(Executor diskIO, Executor networkIO, Executor mainThread, Executor computationThread){
         this.diskIO = diskIO;
         this.networkIO = networkIO;
         this.mainThread = mainThread;
+        this.computationThread = computationThread;
     }
 
     public AppExecutors(){
         this(Executors.newSingleThreadExecutor(), Executors.newFixedThreadPool(THREAD_COUNT),
-                new MainThreadExecutor());
+                new MainThreadExecutor(), Executors.newFixedThreadPool(THREAD_COUNT));
     }
 
     public Executor diskIO(){
@@ -40,8 +45,12 @@ public class AppExecutors{
         return networkIO;
     }
 
-    public Executor mainThreadHelper(){
+    public Executor publishOnUIThread(){
         return mainThread;
+    }
+
+    public Executor computationalThread(){
+        return computationThread;
     }
 
     private static class MainThreadExecutor implements Executor{
