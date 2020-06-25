6e6d0393729f7794ae397420fb2515c6d8901e93
Force Porquillo <fporquillo18@gmail.com>
2020-06-26 03:37:43 +0800

    feat: add api module arguments to network api calls.

--- app/src/main/java/com/force/codes/project/app/data_layer/repositories/map/MapRepositoryImpl.java
+++ app/src/main/java/com/force/codes/project/app/data_layer/repositories/map/MapRepositoryImpl.java
@@ -8,27 +8,39 @@
 package com.force.codes.project.app.data_layer.repositories.map;
 
 import com.force.codes.project.app.data_layer.model.ListData;
+import com.force.codes.project.app.data_layer.resources.api.ApiModule;
 import com.force.codes.project.app.data_layer.resources.api.ApiServiceAdapter;
 import com.force.codes.project.app.data_layer.resources.database.data.MapDao;
+import com.force.codes.project.app.service.executors.AppExecutors;
 
 import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MapRepositoryImpl implements MapRepository{
     private MapDao mapDao;
     private ApiServiceAdapter serviceAdapter;
+    private AppExecutors executors;
 
-    public MapRepositoryImpl(MapDao mapDao, ApiServiceAdapter serviceAdapter){
+    public MapRepositoryImpl(MapDao mapDao, ApiServiceAdapter serviceAdapter, AppExecutors appExecutors){
         this.mapDao = mapDao;
         this.serviceAdapter = serviceAdapter;
+        this.executors = appExecutors;
     }
 
     @Override
     public Flowable<ListData> getListDataFromNetwork(){
-        return null;
+        return serviceAdapter.getResponse(ApiModule.LOCAL_URL);
+    }
+
+    @Override
+    public Flowable<ListData> getDataFromDatabase(){
+        return mapDao.getDataFromNetwork()
+                .subscribeOn(Schedulers.io());
     }
 
     @Override
     public void insertOrUpdate(ListData listData){
-
+        executors.diskIO().execute(() ->
+                mapDao.savePhData(listData));
     }
 }
