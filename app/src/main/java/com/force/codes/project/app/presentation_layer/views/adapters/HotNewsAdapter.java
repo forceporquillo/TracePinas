40be742769f9e401fd533d5dd97608764b946ba2
Force Porquillo <fporquillo18@gmail.com>
2020-07-10 21:34:27 +0800

    feat: updated to pagination + databinding
    converts to pagelist adapter and added diff util method to calculate item changes;
    added databinding in viewholder for interactive UI updates.

--- app/src/main/java/com/force/codes/project/app/presentation_layer/views/adapters/HotNewsAdapter.java
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/views/adapters/HotNewsAdapter.java
@@ -7,47 +7,65 @@
 
 package com.force.codes.project.app.presentation_layer.views.adapters;
 
-import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.databinding.DataBindingUtil;
+import androidx.paging.PagedListAdapter;
+import androidx.recyclerview.widget.DiffUtil;
 
 import com.force.codes.project.app.R;
-import com.force.codes.project.app.data_layer.testmodel.Models;
+import com.force.codes.project.app.data_layer.model.ArticlesItem;
+import com.force.codes.project.app.databinding.HotNewsLayoutBinding;
 import com.force.codes.project.app.presentation_layer.controller.custom.interfaces.NewsItemCallback;
 import com.force.codes.project.app.presentation_layer.views.viewholders.HotNewsViewHolder;
 
-import java.util.ArrayList;
-
-public class HotNewsAdapter extends RecyclerView.Adapter<HotNewsViewHolder>{
-    private ArrayList<Models> hotList;
-    Context context;
+public class HotNewsAdapter extends PagedListAdapter<ArticlesItem, HotNewsViewHolder>{
     private NewsItemCallback callback;
 
-    public HotNewsAdapter(ArrayList<Models> hotList, NewsItemCallback callback){
-        this.hotList = hotList;
+    public HotNewsAdapter(NewsItemCallback callback){
+        super(DIFF_CALLBACK);
         this.callback = callback;
     }
 
     @NonNull
     @Override
-    public HotNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-        final View view = LayoutInflater.from(this.context = parent.getContext())
-                .inflate(R.layout.hot_news_layout, parent, false);
-        return new HotNewsViewHolder(view, callback);
+    public HotNewsViewHolder
+    onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        HotNewsLayoutBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.hot_news_layout, parent, false);
+        return new HotNewsViewHolder(binding, callback);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HotNewsViewHolder holder, int position){
-        Models models = hotList.get(position);
-        holder.setHotView(models);
+        ArticlesItem articlesItem = getArticleItemAt(position);
+
+        if(articlesItem != null){
+            holder.bindTo(articlesItem);
+        }
     }
 
-    @Override
-    public int getItemCount(){
-        return !hotList.isEmpty() ? hotList.size() : 0;
+    final ArticlesItem getArticleItemAt(int position){
+        return getItem(position);
     }
+
+    private static DiffUtil.ItemCallback<ArticlesItem>
+            DIFF_CALLBACK = new DiffUtil.ItemCallback<ArticlesItem>(){
+        @Override
+        public boolean areItemsTheSame(
+                @NonNull ArticlesItem oldItem, @NonNull ArticlesItem newItem
+        ){
+            return oldItem.getContent().equals(newItem.getContent());
+        }
+
+        @Override
+        public boolean areContentsTheSame(
+                @NonNull ArticlesItem oldItem, @NonNull ArticlesItem newItem
+        ){
+            return oldItem.getContent().equals(newItem.getContent());
+        }
+    };
 }
--- app/src/main/java/com/force/codes/project/app/presentation_layer/views/viewholders/HotNewsViewHolder.java
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/views/viewholders/HotNewsViewHolder.java
@@ -12,45 +12,55 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.force.codes.project.app.BR;
 import com.force.codes.project.app.R;
+import com.force.codes.project.app.data_layer.model.ArticlesItem;
+import com.force.codes.project.app.data_layer.model.NewsData;
 import com.force.codes.project.app.data_layer.testmodel.Models;
+import com.force.codes.project.app.databinding.HeaderNewsLayoutBinding;
+import com.force.codes.project.app.databinding.HotNewsLayoutBinding;
 import com.force.codes.project.app.presentation_layer.controller.custom.interfaces.NewsItemCallback;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import timber.log.Timber;
 
 public class HotNewsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-    @BindView(R.id.image_placeholder)
-    ImageView imageView;
 
-    @BindView(R.id.item_title)
-    TextView title;
+    private NewsItemCallback callback;
+    private HotNewsLayoutBinding binding;
 
-    @BindView(R.id.item_subtitle)
-    TextView timestamp;
-
-    NewsItemCallback callback;
-
-    public HotNewsViewHolder(@NonNull View itemView, NewsItemCallback callback){
-        super(itemView);
-        ButterKnife.bind(this, itemView);
+    public HotNewsViewHolder(
+            @NonNull HotNewsLayoutBinding binding,
+            NewsItemCallback callback
+    ){
+        super(binding.getRoot());
+        this.binding = binding;
         this.callback = callback;
         itemView.setOnClickListener(this);
     }
 
-    public void setHotView(Models models){
-        title.setText(models.getTitle());
-        timestamp.setText(models.getDate());
+    public void bindTo(ArticlesItem articlesItem){
+       binding.setArticleItem(articlesItem);
+       binding.setVariable(BR.articleItem, articlesItem);
+       binding.executePendingBindings();
+    }
+
+    @BindingAdapter({"imgUrl"})
+    public static void
+    setThumbnail(ImageView imageView, @NotNull String url){
         Glide.with(imageView.getContext())
-                .load(models.getThumbnail())
-                .apply(new RequestOptions()
-                        .fitCenter()
-                        .override(imageView.getWidth(),
-                                imageView.getHeight()))
+                .load(url)
+                .placeholder(R.drawable.progress_loading)
                 .centerCrop()
                 .into(imageView);
     }
