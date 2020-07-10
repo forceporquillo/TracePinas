70b8d3ed8573e081b3a15cac7aa103b361e9756c
Force Porquillo <fporquillo18@gmail.com>
2020-07-10 23:56:29 +0800

    feat: updated news data

--- app/src/main/java/com/force/codes/project/app/data_layer/model/NewsData.java
+++ app/src/main/java/com/force/codes/project/app/data_layer/model/NewsData.java
@@ -1,58 +1,53 @@
 package com.force.codes.project.app.data_layer.model;
 
-import androidx.room.Entity;
 import androidx.room.PrimaryKey;
-import androidx.room.TypeConverters;
+
+import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-import com.force.codes.project.app.data_layer.TypeConverter.ArticleItemsConverter;
-import com.google.gson.annotations.SerializedName;
-
-@Entity
 public class NewsData{
-	@PrimaryKey(autoGenerate = true)
-	private int id;
+    @PrimaryKey()
+    private int id;
 
-	@SerializedName("totalResults")
-	private int totalResults;
+    @SerializedName("totalResults")
+    private int totalResults;
 
-	@SerializedName("articles")
-	@TypeConverters(ArticleItemsConverter.class)
-	private List<ArticlesItem> articles;
+    @SerializedName("articles")
+    private List<ArticlesItem> articles;
 
-	@SerializedName("status")
-	private String status;
+    @SerializedName("status")
+    private String status;
 
-	public int getTotalResults(){
-		return totalResults;
-	}
+    public int getTotalResults(){
+        return totalResults;
+    }
 
-	public List<ArticlesItem> getArticles(){
-		return articles;
-	}
+    public List<ArticlesItem> getArticles(){
+        return articles;
+    }
 
-	public String getStatus(){
-		return status;
-	}
+    public String getStatus(){
+        return status;
+    }
 
-	public void setId(int id){
-		this.id = id;
-	}
+    public void setId(int id){
+        this.id = id;
+    }
 
-	public void setTotalResults(int totalResults){
-		this.totalResults = totalResults;
-	}
+    public void setTotalResults(int totalResults){
+        this.totalResults = totalResults;
+    }
 
-	public void setArticles(List<ArticlesItem> articles){
-		this.articles = articles;
-	}
+    public void setArticles(List<ArticlesItem> articles){
+        this.articles = articles;
+    }
 
-	public void setStatus(String status){
-		this.status = status;
-	}
+    public void setStatus(String status){
+        this.status = status;
+    }
 
-	public int getId(){
-		return id;
-	}
+    public int getId(){
+        return id;
+    }
 }
