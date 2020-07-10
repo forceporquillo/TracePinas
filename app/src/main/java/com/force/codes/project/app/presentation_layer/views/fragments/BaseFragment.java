01d7d17ac13253b80d0a427395fe0c1a4e349186
Force Porquillo <fporquillo18@gmail.com>
2020-07-10 23:59:25 +0800

    Update BaseFragment.java

--- app/src/main/java/com/force/codes/project/app/presentation_layer/views/fragments/BaseFragment.java
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/views/fragments/BaseFragment.java
@@ -6,24 +6,21 @@
 
 package com.force.codes.project.app.presentation_layer.views.fragments;
 
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.force.codes.project.app.R;
 
-public abstract class BaseFragment extends Fragment{
-    public FragmentTransaction setDelegateFragment(Fragment fragment){
+import dagger.android.support.DaggerFragment;
+
+public abstract class BaseFragment extends DaggerFragment{
+    protected FragmentTransaction setFragment(Fragment fragment){
         FragmentManager fragmentManager = getParentFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-        if(fragment instanceof StatisticsFragment){
-            transaction.setCustomAnimations(
-                    R.anim.enter_from_right, R.anim.exit_to_left,
-                    R.anim.enter_from_left, R.anim.exit_to_right);
-        }
-
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left,
+                R.anim.enter_from_left, R.anim.exit_to_right);
         return transaction.replace(R.id.fragment_container, fragment)
                 .addToBackStack(fragment.getTag());
     }
