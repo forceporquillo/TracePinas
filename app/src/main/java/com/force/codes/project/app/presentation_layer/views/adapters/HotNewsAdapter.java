ca56f8b6e715f9224358cb97a4d692954c7ea57f
Force Porquillo <fporquillo18@gmail.com>
2020-07-15 23:20:28 +0800

    fit: added diff util

--- app/src/main/java/com/force/codes/project/app/presentation_layer/views/adapters/HotNewsAdapter.java
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/views/adapters/HotNewsAdapter.java
@@ -11,22 +11,33 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.paging.PagedListAdapter;
 import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.force.codes.project.app.R;
-import com.force.codes.project.app.data_layer.model.ArticlesItem;
+import com.force.codes.project.app.data_layer.model.news.ArticlesItem;
 import com.force.codes.project.app.databinding.HotNewsLayoutBinding;
 import com.force.codes.project.app.presentation_layer.controller.custom.interfaces.NewsItemCallback;
 import com.force.codes.project.app.presentation_layer.views.viewholders.HotNewsViewHolder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotNewsAdapter extends PagedListAdapter<ArticlesItem, HotNewsViewHolder>{
     private NewsItemCallback callback;
+    private List<ArticlesItem> itemList;
 
     public HotNewsAdapter(NewsItemCallback callback){
         super(DIFF_CALLBACK);
         this.callback = callback;
+        itemList = new ArrayList<>();
+    }
+
+    public void setItemList(List<ArticlesItem> itemList){
+        this.itemList = itemList;
     }
 
     @NonNull
@@ -34,38 +45,31 @@ public class HotNewsAdapter extends PagedListAdapter<ArticlesItem, HotNewsViewHo
     public HotNewsViewHolder
     onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        HotNewsLayoutBinding binding = DataBindingUtil.inflate(
-                inflater, R.layout.hot_news_layout, parent, false);
+        HotNewsLayoutBinding binding = DataBindingUtil.inflate(inflater,
+                R.layout.hot_news_layout, parent, false);
         return new HotNewsViewHolder(binding, callback);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HotNewsViewHolder holder, int position){
         ArticlesItem articlesItem = getArticleItemAt(position);
-
-        if(articlesItem != null){
-            holder.bindTo(articlesItem);
-        }
+        holder.bindTo(articlesItem);
     }
 
     final ArticlesItem getArticleItemAt(int position){
         return getItem(position);
     }
 
-    private static DiffUtil.ItemCallback<ArticlesItem>
-            DIFF_CALLBACK = new DiffUtil.ItemCallback<ArticlesItem>(){
+    private static DiffUtil.ItemCallback<ArticlesItem> DIFF_CALLBACK = new DiffUtil
+            .ItemCallback<ArticlesItem>(){
         @Override
-        public boolean areItemsTheSame(
-                @NonNull ArticlesItem oldItem, @NonNull ArticlesItem newItem
-        ){
-            return oldItem.getContent().equals(newItem.getContent());
+        public boolean areItemsTheSame(@NonNull ArticlesItem oldItem, @NonNull ArticlesItem newItem){
+            return oldItem.getPublishedAt().equals(newItem.getPublishedAt());
         }
 
         @Override
-        public boolean areContentsTheSame(
-                @NonNull ArticlesItem oldItem, @NonNull ArticlesItem newItem
-        ){
-            return oldItem.getContent().equals(newItem.getContent());
+        public boolean areContentsTheSame(@NonNull ArticlesItem oldItem, @NonNull ArticlesItem newItem){
+            return oldItem.getTitle().equals(newItem.getTitle());
         }
     };
 }
