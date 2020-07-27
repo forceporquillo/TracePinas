198c7af7c88ac7f7541db403234934a8ac1bf575
Force Porquillo <fporquillo18@gmail.com>
2020-07-27 12:01:38 +0800

    added callback listeners for fragment and recyclerview

---
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/controller/interfaces/FragmentCallback.java
@@ -0,0 +1,13 @@
+package com.force.codes.project.app.presentation_layer.controller.interfaces;
+
+
+/*
+ * Created by Force Porquillo on 6/2/20 1:49 PM
+ * Copyright (c) 2020.  All rights reserved.
+ * Last modified 6/2/20 12:55 PM
+ *
+ */
+
+public interface FragmentCallback{
+    void cardItemListener(int position);
+}
