33f814ab6af90d24c952e34c574cf8534427209d
Force Porquillo <fporquillo18@gmail.com>
2020-07-28 17:30:54 +0800

    Create Test.kt

---
+++ app/src/main/java/com/force/codes/project/app/Test.kt
@@ -0,0 +1,12 @@
+/*
+ * Created by Force Porquillo on 7/28/20 5:29 PM
+ * FEU Institute of Technology
+ * Copyright (c) 2020.  All rights reserved.
+ * Last modified 7/28/20 5:29 PM
+ */
+
+package com.force.codes.project.app
+
+fun test(){
+
+}
