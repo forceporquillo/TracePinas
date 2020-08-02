b6911fbe149d3374ddf17d9325e89ad6127b1806
Force Porquillo <fporquillo18@gmail.com>
2020-08-02 23:17:48 +0800

    migrated to Kotlin

---
+++ app/src/main/java/com/force/codes/project/app/data_layer/converters/ArticleItemsConverter.kt
@@ -0,0 +1,31 @@
+/*
+ * Created by Force Porquillo on 7/9/20 5:20 PM
+ * FEU Institute of Technology
+ * Copyright (c) 2020.  All rights reserved.
+ * Last modified 7/9/20 5:20 PM
+ */
+package com.force.codes.project.app.data_layer.converters
+
+import androidx.room.TypeConverter
+import com.force.codes.project.app.data_layer.model.news.ArticlesItem
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+object ArticleItemsConverter {
+  @TypeConverter
+  @JvmStatic fun fromListOfStrings(
+    value: String?
+  ): List<ArticlesItem>? = value.let{
+    val listType =
+      object : TypeToken<List<ArticlesItem?>?>() {
+      }.type
+    return Gson().fromJson(value, listType)
+  }
+
+  @TypeConverter
+  @JvmStatic fun toListOfStrings(
+    objects: List<ArticlesItem?>?
+  ): String {
+    return Gson().toJson(objects)
+  }
+}
