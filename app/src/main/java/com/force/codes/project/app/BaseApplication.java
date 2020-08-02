7c45d04083b7019531236e050c0b63a5159c9739
Force Porquillo <fporquillo18@gmail.com>
2020-08-02 23:13:24 +0800

    fix: debug modules

--- app/src/main/java/com/force/codes/project/app/BaseApplication.java
+++ app/src/main/java/com/force/codes/project/app/BaseApplication.java
@@ -8,53 +8,51 @@
 package com.force.codes.project.app;
 
 import android.app.Application;
-import android.os.Build;
-
-import com.force.codes.project.app.app.debug.DebugTreeApplication;
+import com.force.codes.project.app.app.debug.DebugTreeApp;
+import com.force.codes.project.app.app.debug.DetectLeak;
 import com.force.codes.project.app.app.di.AppComponent;
 import com.force.codes.project.app.app.di.DaggerAppComponent;
 import com.force.codes.project.app.app.di.module.AppModule;
-
-import javax.inject.Inject;
-
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasAndroidInjector;
+import javax.inject.Inject;
 import timber.log.Timber;
 
-import static com.force.codes.project.app.app.debug.LeakDetection.startLeakDetection;
+public class BaseApplication extends Application implements HasAndroidInjector {
+  @Inject
+  DispatchingAndroidInjector<Object> dispatchingAndroidInjector;
 
-public class BaseApplication extends Application implements HasAndroidInjector{
-    @Inject
-    DispatchingAndroidInjector<Object> dispatchingAndroidInjector;
+  private AppComponent appComponent;
 
-    private AppComponent appComponent;
-
-    private static void setDebugInstance(DebugTreeApplication debugInstance){
-        if(BuildConfig.DEBUG){
-            debugInstance.DebugTree();
-            startLeakDetection();
-        }else Timber.plant(new DebugTreeApplication.CrashReportingTree());
+  private static void setDebugInstance() {
+    if (BuildConfig.DEBUG) {
+      DebugTreeApp.debug();
+      DetectLeak.enabledStrictMode();
+      DetectLeak.startLeak();
+    } else {
+      Timber.plant(new DebugTreeApp.CrashReportingTree());
     }
+  }
 
-    @Override
-    public void onCreate(){
-        super.onCreate();
-        appComponent = DaggerAppComponent.builder()
-                .application(this)
-                .appModule(new AppModule(this))
-                .build();
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    appComponent = DaggerAppComponent.builder()
+        .application(this)
+        .appModule(new AppModule(this))
+        .build();
 
-        appComponent.inject(this);
-        setDebugInstance(new DebugTreeApplication(this));
-    }
+    appComponent.inject(this);
+    setDebugInstance();
+  }
 
-    public AppComponent getAppComponent(){
-        return appComponent;
-    }
+  public AppComponent getAppComponent() {
+    return appComponent;
+  }
 
-    @Override
-    public AndroidInjector<Object> androidInjector(){
-        return dispatchingAndroidInjector;
-    }
+  @Override
+  public AndroidInjector<Object> androidInjector() {
+    return dispatchingAndroidInjector;
+  }
 }
