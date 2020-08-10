6900cabe008c4ec2a1248b13dc310c233ba0fcb3
Force Porquillo <fporquillo18@gmail.com>
2020-08-10 22:21:12 +0800

    added toolbar menu icons and callback listeners for listview

--- app/src/main/java/com/force/codes/project/app/presentation_layer/views/activity/list_component/ListViewActivity.java
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/views/activity/list_component/ListViewActivity.java
@@ -8,16 +8,16 @@
 package com.force.codes.project.app.presentation_layer.views.activity.list_component;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import com.force.codes.project.app.BR;
 import com.force.codes.project.app.R;
 import com.force.codes.project.app.data_layer.model.country.CountryDetails;
 import com.force.codes.project.app.databinding.ActivityListViewBinding;
-import com.force.codes.project.app.presentation_layer.controller.interfaces.ListItemListener;
 import com.force.codes.project.app.presentation_layer.controller.interfaces.ListViewCallback;
 import com.force.codes.project.app.presentation_layer.controller.utils.ItemDecoration;
 import com.force.codes.project.app.presentation_layer.views.activity.BaseActivity;
@@ -26,7 +26,6 @@ import com.force.codes.project.app.presentation_layer.views.fragments.viewpager.
 import com.force.codes.project.app.presentation_layer.views.viewmodels.ListViewModel;
 import java.util.List;
 import javax.inject.Inject;
-import timber.log.Timber;
 
 public class ListViewActivity extends BaseActivity implements ListViewCallback {
   private ActivityListViewBinding binding;
@@ -40,38 +39,56 @@ public class ListViewActivity extends BaseActivity implements ListViewCallback {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_list_view);
     binding = DataBindingUtil.setContentView(this, R.layout.activity_list_view);
-
     binding.setListActivity(this);
     binding.setLifecycleOwner(this);
     binding.setVariable(BR.listActivity, this);
-
     viewModel = new ViewModelProvider(this, factory).get(ListViewModel.class);
-
     binding.toolbar.setTitle("");
     setSupportActionBar(binding.toolbar);
-
     if (getSupportActionBar() != null) {
       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
       getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
-
-    binding.executePendingBindings();
+    if (binding.hasPendingBindings()) {
+      binding.executePendingBindings();
+    }
   }
 
   @Override protected void onStart() {
     super.onStart();
-    viewModel.getCountryData().observe(this, countryDetails -> {
-      details = countryDetails;
-      setRecyclerView(countryDetails);
-    });
+    viewModel.getCountryData(true).observe(this, countryDetails ->
+        setRecyclerView(details = countryDetails));
   }
 
-  private void setRecyclerView(final List<CountryDetails> details) {
+  final void setRecyclerView(final List<CountryDetails> details) {
     binding.listRecycler.setLayoutManager(new LinearLayoutManager(this));
-    binding.listRecycler.addItemDecoration(new ItemDecoration(
-        this, ItemDecoration.VERTICAL_LIST, 0));
+    binding.listRecycler.addItemDecoration(new ItemDecoration(this,
+        ItemDecoration.VERTICAL_LIST, 0));
     binding.listRecycler.setAdapter(new ListAdapter(details, this));
     binding.invalidateAll();
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_items, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    boolean order = true;
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        this.onBackPressed();
+        return true;
+      case R.id.action_cases:
+        order = true;
+        break;
+      case R.id.action_az:
+        order = false;
+        break;
+    }
+    viewModel.getCountryData(order);
+    binding.invalidateAll();
+    return super.onOptionsItemSelected(item);
   }
 
   @Override protected void onDestroy() {
@@ -86,17 +103,13 @@ public class ListViewActivity extends BaseActivity implements ListViewCallback {
     }
   }
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == android.R.id.home) {
-      this.onBackPressed();
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
-
+  /**
+   * We only need to get the adapter position of a clicked item in a listView
+   * to dereference the String {@link LiveData} from DB and emit this item to
+   * {@link MyCountryFragment} and finish activity.
+   */
   @Override public void getPosition(int position) {
     viewModel.insertSelectedCountry(details.get(position).getCountry());
-    finish();
+    this.finish();
   }
 }
