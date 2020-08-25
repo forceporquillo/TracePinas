d5867e21840ae558dd0d869e4790c6c10dfae0ea
Force Porquillo <fporquillo18@gmail.com>
2020-08-26 06:21:02 +0800

    Add TopRegions RecyclerView adapter and viewholder

---
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/views/adapters/TopRegionsAdapter.java
@@ -0,0 +1,37 @@
+package com.force.codes.project.app.presentation_layer.views.adapters;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.force.codes.project.app.data_layer.model.philippines.DataItem;
+import com.force.codes.project.app.data_layer.model.philippines.TopRegions;
+import com.force.codes.project.app.databinding.LayoutTopRegionsBinding;
+import com.force.codes.project.app.presentation_layer.views.viewholders.TopRegionsViewHolder;
+import java.util.List;
+
+public class TopRegionsAdapter extends RecyclerView.Adapter<TopRegionsViewHolder> {
+  private final List<DataItem> data;
+
+  public TopRegionsAdapter(List<DataItem> data) {
+    this.data = data;
+  }
+
+  @NonNull @Override
+  public TopRegionsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+    LayoutTopRegionsBinding binding = LayoutTopRegionsBinding
+        .inflate(inflater, parent, false);
+    return new TopRegionsViewHolder(binding);
+  }
+
+  @Override public void onBindViewHolder(@NonNull TopRegionsViewHolder holder, int position) {
+    DataItem item = data.get(position);
+
+    holder.setBinding(item);
+  }
+
+  @Override public int getItemCount() {
+    return data.size();
+  }
+}
