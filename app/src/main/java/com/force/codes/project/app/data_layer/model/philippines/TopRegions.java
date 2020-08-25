d48670cbdce9081a5e827bdd6a24d1cfb211b3f5
Force Porquillo <fporquillo18@gmail.com>
2020-08-26 06:20:34 +0800

    Add TopRegions affected by COVID19 model

---
+++ app/src/main/java/com/force/codes/project/app/data_layer/model/philippines/TopRegions.java
@@ -0,0 +1,21 @@
+package com.force.codes.project.app.data_layer.model.philippines;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+public class TopRegions{
+
+	@SerializedName("data")
+	private List<DataItem> data;
+
+  public TopRegions(List<DataItem> data) {
+  }
+
+  public void setData(List<DataItem> data){
+		this.data = data;
+	}
+
+	public List<DataItem> getData(){
+		return data;
+	}
+}
