fc35d7e934161172ff1247afb37e6c3722ee18ad
Force Porquillo <fporquillo18@gmail.com>
2020-08-26 19:09:41 +0800

    Move group to navigation package folder

--- app/src/main/java/com/force/codes/project/app/presentation_layer/controller/navigation/BottomBarGroup.kt
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/controller/navigation/BottomBarGroup.kt
@@ -4,9 +4,9 @@
  * Copyright (c) 2020.  All rights reserved.
  * Last modified 7/6/20 2:19 AM
  */
-package com.force.codes.project.app.presentation_layer.controller.model
+package com.force.codes.project.app.presentation_layer.controller.navigation
 
-class Group {
+class BottomBarGroup {
   var groupTitle: String? = null
   var isSpinner = false
 
