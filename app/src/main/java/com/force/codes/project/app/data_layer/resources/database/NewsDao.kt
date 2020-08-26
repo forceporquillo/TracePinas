0de1fac54b7bf4afeb91d1cc3b2acb7d559dda1f
Force Porquillo <fporquillo18@gmail.com>
2020-08-26 19:13:06 +0800

    Fix news dao

--- app/src/main/java/com/force/codes/project/app/data_layer/resources/database/NewsDao.kt
+++ app/src/main/java/com/force/codes/project/app/data_layer/resources/database/NewsDao.kt
@@ -14,24 +14,14 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import androidx.room.TypeConverters
-import com.force.codes.project.app.app.constants.DatabaseConstants.QUERY_ARTICLES_ITEM_LIMIT
-import com.force.codes.project.app.app.constants.DatabaseConstants.QUERY_TWITTER_DATA
-import com.force.codes.project.app.data_layer.resources.database.converters.TwitterMediaConverter
 import com.force.codes.project.app.data_layer.model.news.ArticlesItem
 import com.force.codes.project.app.data_layer.model.twitter.TwitterData
+import com.force.codes.project.app.data_layer.resources.database.converters.TwitterMediaConverter
 
 @Dao
 interface NewsDao {
-  @get:Transaction
-  @get:Query(QUERY_ARTICLES_ITEM_LIMIT)
-  val newsDataFromDatabase: Factory<Int?, ArticlesItem?>?
-
-  @Insert(onConflict = OnConflictStrategy.REPLACE)
-  fun insertOrUpdateArticleItems(
-    items: List<ArticlesItem?>?
+  @get:Query("SELECT * FROM TwitterData ORDER BY id DESC"
   )
-
-  @get:Query(QUERY_TWITTER_DATA)
   @get:TypeConverters(TwitterMediaConverter::class)
   @get:Transaction
   val recentTweetsFromDatabase: Factory<Int?, TwitterData?>?
@@ -39,5 +29,16 @@ interface NewsDao {
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   fun insertOrUpdateTwitterUsers(
     userData: List<TwitterData?>?
+  )
+
+  @get:Transaction
+  @get:Query(
+      "SELECT * FROM ArticlesItem ORDER BY publishedAt DESC LIMIT 100"
+  )
+  val newsDataFromDatabase: Factory<Int?, ArticlesItem?>?
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun insertOrUpdateArticleItems(
+    items: List<ArticlesItem?>?
   )
 }
