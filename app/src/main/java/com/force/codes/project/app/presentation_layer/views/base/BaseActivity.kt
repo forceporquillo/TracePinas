686493a06077d5d77b09ead231bbb2f6f8d18259
Force Porquillo <fporquillo18@gmail.com>
2020-08-31 04:06:33 +0800

    Update BaseActivity.kt

--- app/src/main/java/com/force/codes/project/app/presentation_layer/views/base/BaseActivity.kt
+++ app/src/main/java/com/force/codes/project/app/presentation_layer/views/base/BaseActivity.kt
@@ -5,18 +5,16 @@ import android.os.Bundle
 import com.force.codes.project.app.BuildConfig
 import com.force.codes.project.app.presentation_layer.controller.service.ThreadExecutor
 import dagger.android.support.DaggerAppCompatActivity
-import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
-
-const val SHARED_PREF: String = "SHARED_PREF"
-const val PREF_VERSION_CODE_KEY = "VERSION_CODE"
-const val NOT_EXIST = -1
-const val VERSION_CODE = BuildConfig.VERSION_CODE
 
 abstract class BaseActivity : DaggerAppCompatActivity() {
 
   companion object {
     @JvmStatic var isFreshInstalled: Boolean = false
+    const val SHARED_PREF: String = "SHARED_PREF"
+    const val PREF_VERSION_CODE_KEY = "VERSION_CODE"
+    const val NOT_EXIST = -1
+    const val VERSION_CODE = BuildConfig.VERSION_CODE
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,25 +25,26 @@ abstract class BaseActivity : DaggerAppCompatActivity() {
   }
 
   private fun checkIfFreshInstall() {
-    ThreadExecutor(0).diskIO().execute {
-      val pref: SharedPreferences = getSharedPreferences(SHARED_PREF, MODE_PRIVATE)
-      val currentSaveVersion = pref.getInt(PREF_VERSION_CODE_KEY, NOT_EXIST)
+    ThreadExecutor(0).diskIO()
+        .execute {
+          val pref: SharedPreferences = getSharedPreferences(SHARED_PREF, MODE_PRIVATE)
+          val currentSaveVersion = pref.getInt(PREF_VERSION_CODE_KEY, NOT_EXIST)
 
-      when {
-        VERSION_CODE == currentSaveVersion -> {
-          return@execute
+          when {
+            VERSION_CODE == currentSaveVersion -> {
+              return@execute
+            }
+            currentSaveVersion == NOT_EXIST -> {
+              Timber.e("First run")
+              isFreshInstalled = true
+            }
+            VERSION_CODE > currentSaveVersion -> {
+              Timber.e("Upgrade")
+            }
+          }
+          pref.edit()
+              .putInt(PREF_VERSION_CODE_KEY, VERSION_CODE)
+              .apply()
         }
-        currentSaveVersion == NOT_EXIST -> {
-          Timber.e("First run")
-          isFreshInstalled = true
-        }
-        VERSION_CODE > currentSaveVersion -> {
-          Timber.e("Upgrade")
-        }
-      }
-      pref.edit()
-          .putInt(PREF_VERSION_CODE_KEY, VERSION_CODE)
-          .apply()
-    }
   }
 }
